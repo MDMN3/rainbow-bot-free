2bd93820184f9821d028957a48bdc2bf58ba6eee
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2019-03-22 11:29:21 +0400

    Update rainbow.js

--- Commands/rainbow.js
+++ Commands/rainbow.js
@@ -5,7 +5,7 @@ module.exports.info = {
 };
 
 module.exports.run = (message) => {
-    if (!message.member.hasPermission('MANAGE_ROLES')) return Bot.err(message, null, 'Manage roles');
+    /*if (!message.member.hasPermission('MANAGE_ROLES')) return Bot.err(message, null, 'Manage roles');
     if (!Bot.stop.has(message.guild.id)) return Bot.err(message, 'Rainbow is already started');
 
     const role = message.guild.roles.find(r => r.name.match(/multicolor/i));
@@ -18,5 +18,6 @@ module.exports.run = (message) => {
     .setDescription('Rainbow started succesfully')
     .setColor(Bot.colors.blurple)
     .setFooter(Bot.footer);
-    message.channel.send(embed);
+    message.channel.send(embed);*/
+    message.channel.send('В связи с тем что Discord ввел новые ограничения на изменения роли, этот бот больше не будет работать, точно также умерла и вся индустрия радужных ботов. Если вы заплатили за бот деньги, то пожалуйста, свяжитесь с `ANDREY#2623`, чтобы вернуть их. Мне очень жаль что это произошло. Земля тебе пухом, Colorful\n\nПолезные ссылки: Наш бот-модератор <https://discordapp.com/oauth2/authorize?client_id=522019477408448524&scope=bot&permissions=8>\nНаш бот с крестиками-ноликами и другими минииграми: <https://discordapp.com/oauth2/authorize?client_id=522731595858313217&scope=bot&permissions=379904>')
 };
